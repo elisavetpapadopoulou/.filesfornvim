3cf91a4e303e62e3717625d745c0a3580e870bff
elisavet papadopoulou <95046696+elisavetpapadopoulou@users.noreply.github.com>
2025-10-02 15:43:03 +0300

    Update lsp.lua

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,18 +1,32 @@
-require("nvim-lsp-installer").setup {}
 require("mason").setup()
-require'lspconfig'.rust_analyzer.setup{}
-require'lspconfig'.clangd.setup{}
--- require'lspconfig'.java_language_server.setup{}
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.pyre.setup{}
-require'lspconfig'.tsserver.setup{}
-require'lspconfig'.cssls.setup{}
-require'lspconfig'.html.setup{}
-require'lspconfig'.lua_ls.setup {}
--- require'lspconfig'.millet.setup{}
-require'lspconfig'.sqlls.setup{}
-require'lspconfig'.bashls.setup{}
-require'lspconfig'.vimls.setup{}
-require'lspconfig'.golangci_lint_ls.setup{}
-require'lspconfig'.gopls.setup{}
-require'lspconfig'.jdtls.setup{}
+vim.lsp.config('rust_analyzer', {})
+vim.lsp.config('sourcekit', {})
+vim.lsp.config('clangd', {})
+-- vim.lsp.config('java_language_server', {})
+vim.lsp.config('pyright', {})
+vim.lsp.config('pyre', {})
+vim.lsp.config('ts_ls', {})         
+vim.lsp.config('cssls', {})
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+vim.lsp.config('html', { capabilities = capabilities })
+vim.lsp.config('lua_ls', {})
+-- vim.lsp.config('millet', {})
+vim.lsp.config('sqlls', {})
+vim.lsp.config('bashls', {})
+vim.lsp.config('vimls', {})
+vim.lsp.config('golangci_lint_ls', {})
+vim.lsp.config('gopls', {})
+vim.lsp.config('jdtls', {})
+vim.lsp.config('biome', {})
+vim.lsp.config('gdscript', { filetypes = { "gd", "gdscript", "gdscript3" } })
+
+for _, s in ipairs({
+  'rust_analyzer','sourcekit','clangd','pyright','pyre','ts_ls','cssls',
+  'html','lua_ls','sqlls','bashls','vimls','golangci_lint_ls',
+  'gopls','jdtls','biome','gdscript'
+}) do
+  pcall(vim.lsp.enable, s)
+end
